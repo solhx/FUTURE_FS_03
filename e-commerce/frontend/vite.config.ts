616b4630bf2489a4545371c4f19df1c3d7f47f62
Hossam Hassan <hossamhassan112003@outlook.com>
2026-03-15 04:30:52 +0200

    done

--- e-commerce/frontend/vite.config.ts
+++ e-commerce/frontend/vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:3000",
+        target: process.env.VITE_API_URL ,
         changeOrigin: true,
       },
       "/uploads": {
-        target: process.env.VITE_API_URL || "http://localhost:3000",
+        target: process.env.VITE_API_URL ,
         changeOrigin: true,
       },
     },
